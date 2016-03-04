7ed408670296d829492cef37955f961efc3884f9
ZacMarcus <ZacMarcus@users.noreply.github.com>
2016-03-04 23:14:53 +1000

    renamed class

--- YahooFinance.NET/YahooFinance.cs
+++ YahooFinance.NET/YahooFinance.cs
@@ -7,7 +7,7 @@ namespace YahooFinance.NET
 {
 	// This project can output the Class library as a NuGet Package.
 	// To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-	public class YahooFinance
+	public class YahooFinanceClient
 	{
 		private const int MinimumDateRangeDays = -30;
 
