4d3b13219715fe11b11d97e58f6e4765fdd613ee
ZacMarcus <ZacMarcus@users.noreply.github.com>
2016-04-03 16:01:21 +1000

    added a unit test

--- YahooFinance.NET.Tests/GetYahooStockCodeTests.cs
+++ YahooFinance.NET.Tests/GetYahooStockCodeTests.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace YahooFinance.NET.Tests
 {
 	// This project can output the Class library as a NuGet Package.
 	// To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-	public class Class1
+	public class GetYahooStockCodeTests
 	{
-		public Class1()
+		public GetYahooStockCodeTests()
 		{
 			
+		}
+
+		[Fact]
+		public void TestUppercase()
+		{
+			var x = new YahooFinanceClient();
+			var yahooStockCode = x.GetYahooStockCode("ASX", "AFI");
+			Assert.Equal("AFI.AX", yahooStockCode);
 		}
 	}
 }
