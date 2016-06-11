243b0c9e755372f10236ffc28306bda3c331a9ec
ZacMarcus <ZacMarcus@users.noreply.github.com>
2016-06-11 23:23:55 +1000

    Update AssemblyInfo

--- YahooFinance.NET/Properties/AssemblyInfo.cs
+++ YahooFinance.NET/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YahooFinance.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Download historical end of day stock data and historical dividend data via the Yahoo Finance API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/ZacMarcus")]
 [assembly: AssemblyProduct("YahooFinance.NET")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
