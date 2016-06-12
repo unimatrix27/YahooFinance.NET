02ff66c6074a73d41cd1a1d339ee1aa419c69734
ZacMarcus <ZacMarcus@users.noreply.github.com>
2016-06-12 12:11:21 +1000

    Added unit tests back in

---
+++ YahooFinance.NET.Tests/GetYahooStockCodeTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+
+namespace YahooFinance.NET.Tests
+{
+	public class GetYahooStockCodeTests
+	{
+		[Fact]
+		public void TestUppercase()
+		{
+			var yahooFinanceClient = new YahooFinanceClient();
+			var yahooStockCode = yahooFinanceClient.GetYahooStockCode("ASX", "AFI");
+			Assert.Equal("AFI.AX", yahooStockCode);
+		}
+
+		[Fact]
+		public void TestLowerCaseExchange()
+		{
+			var yahooFinanceClient = new YahooFinanceClient();
+			var yahooStockCode = yahooFinanceClient.GetYahooStockCode("asx", "AFI");
+			Assert.Equal("AFI.AX", yahooStockCode);
+		}
+
+		[Fact]
+		public void TestLowerCaseSymbol()
+		{
+			var yahooFinanceClient = new YahooFinanceClient();
+			var yahooStockCode = yahooFinanceClient.GetYahooStockCode("ASX", "afi");
+			Assert.Equal("AFI.AX", yahooStockCode);
+		}
+
+		[Fact]
+		public void TestInvalidExchange()
+		{
+			var exchange = "ABC";
+			var yahooFinanceClient = new YahooFinanceClient();
+			var exception = Assert.Throws<Exception>(() => yahooFinanceClient.GetYahooStockCode("ABC", "AFI"));
+			Assert.Equal($"The \"{exchange}\" exchange is not supported.", exception.Message);
+		}
+	}
+}
